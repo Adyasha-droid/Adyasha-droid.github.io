67722a99e7b1487824f39fc817d4341e90df53a9
Adyasha-droid <60071446+Adyasha-droid@users.noreply.github.com>
2021-03-12 20:56:35 +0530

    Add files via upload

---
+++ integrator.js
@@ -0,0 +1,7 @@
+processGoogleToken({
+    "newToken": "ChAIgL-sggYQlfGhsL7wmr45EkgA7d1iqFUwKeX9QEaeXw3nW7Nw7ep-BVTlwXERWYMKz5KSgtXNZT9MZu4IZxbX3bJxF-n8pPdNTM-lkhCd3oOCm4-lCXoaMi0",
+    "validLifetimeSecs": 300,
+    "freshLifetimeSecs": 300,
+    "1p_jar": "",
+    "pucrd": ""
+});
